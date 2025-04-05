7ffdb1fe8d2d01fe18f8b9c8b8f140f40bb26947
vinayboss9669 <vinayboss9669@gmail.com>
2025-04-05 23:12:56 +0530

    First Commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,13 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
+
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    
+    syncUserCreation,
+    syncUserUpdate,
+    syncUserDeletion
+  ],
+});
