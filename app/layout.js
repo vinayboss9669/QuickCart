5160eaa2b914a1ad1eaa999264f4fcdbb11e2813
vinayboss9669 <vinayboss9669@gmail.com>
2025-04-06 15:13:29 +0530

    second

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,11 @@
-import { Outfit } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Outfit } from "next/font/google";
 
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] });
+
 
 export const metadata = {
   title: "QuickCart - GreatStack",
@@ -13,15 +14,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
-          <Toaster />
+    <html lang="en">
+      <body className={outfit.className}>
+        <ClerkProvider>
           <AppContextProvider>
             {children}
+            <Toaster position="top-center" />
           </AppContextProvider>
-        </body>
-      </html>
-      </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
+
+
